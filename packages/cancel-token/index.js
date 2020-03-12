56d160fbbe23fdec166a2f3b48b90da5a301e4f8
taku13 <takumiao13@outlook.com>
2020-03-12 14:54:11 +0800

    cancel-token: add throwIfRequested method

--- packages/cancel-token/index.js
+++ packages/cancel-token/index.js
@@ -6,15 +6,28 @@ function CancelToken(executor) {
     _resolvePromise = resolve;
   });
 
-  executor((message) => {
+  function cancel(message) {
+    // Cancellation has already been requested
+    if (token.reason) {
+      return;
+    }
+
     const cancel = {
       message,
       __CANCEL__: true
     };
     token.reason = cancel; 
     _resolvePromise(token.reason);
-  });
+  }
+
+  executor(cancel);
 }
+
+CancelToken.prototype.throwIfRequested = function() {
+  if (this.reason) {
+    throw this.reason;
+  }
+};
 
 CancelToken.source = function() {
   let cancel;

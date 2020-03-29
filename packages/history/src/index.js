6d7a4acbb0a88c2e6fdab44a51269e9b4a73c920
taku13 <takumiao13@outlook.com>
2020-03-29 14:16:04 +0800

    history: implement it

--- packages/history/src/index.js
+++ packages/history/src/index.js
@@ -1,102 +1,20 @@
 
-var History = function(options) {
-  this.options = options || {};
-  this.root = this.options.root || '/';
-  this._useHash = this.options.mode === 'hash';
-  this.listeners = [];
+import { BrowserHistory } from './BrowserHistory';
+import { HashHistory } from './HashHistory';
 
-  this.location = window.location;
-  this.history = window.history;
-
-  this.handleChange = this.handleChange.bind(this);
-}
-
-History.prototype.subscribe = function(listener) {
-  var self = this;
-  
-  this.listeners.push(listener);
-  this._checkListener(1)
-  return function unsubscribe() {
-    self.listeners = self.listeners.filter(item => item !== listener);
-    self._checkListener(-1);
+const createHistory = (options) => {
+  const { mode = 'browser', ...opts } = options;
+  let history;
+  switch (mode) {
+    case 'hash':
+      history = new HashHistory(opts);
+      break;
+    case 'browser':
+      history = new BrowserHistory(opts);
+      break;
   }
+
+  return history;
 }
 
-History.prototype._checkListener = function(delta) {
-  if (this.listeners.length === 1 && delta === 1) {
-    if (this._useHash) {
-      window.addEventListener('hashchange', this.handleChange, false);
-    } else {
-      window.addEventListener('popstate', this.handleChange, false);
-    }
-  } else if (this.listeners.length === 0) {
-    this.stop();
-  }
-}
-
-History.prototype.stop = function() {
-  if (this._useHash) {
-    window.removeEventListener('hashchange', this.handleChange, false);
-  } else {
-    window.removeEventListener('popstate', this.handleChange, false);
-  }
-}
-
-History.prototype.handleChange = function(e) {
-  this.listeners.forEach(function(listener) {
-    listener(e);
-  });
-}
-
-History.prototype.getSearch = function() {
-  return this.location.search.slice(1);
-}
-
-History.prototype.getHash = function() {
-  return this.location.hash.slice(1);
-}
-
-History.prototype.getPath = function() {
-  // trim root path
-  var path = this.location.pathname.slice(this.root.length - 1) + this.getSearch()
-  // trim head '/'
-  return path.charAt(0) == '/' ? path.slice(1) : path;
-}
-
-History.prototype.getFragment = function() {
-  var fragment;
-  if (this._useHash) {
-    fragment = this.getPath();
-  } else {
-    fragment = this.getHash();
-  }
-  return fragment;
-}
-
-History.prototype.push = function(fragment, options = {}) {
-  var { replace = false } = options;
-  var url = this.root + fragment;
-
-  if (this._useHash) {
-    if (replace) {
-      location.replace(stripHash(location.href) + '#' + fragment);
-    } else {
-      this.location.hash = fragment;
-    }
-  } else {
-    this.history[replace ? 'replaceState' : 'pushState']({}, document.title, url);
-  }
-}
-
-History.prototype.go = function(n) {
-  this.history.go(n)
-}
-
-History.prototype.back = function() {
-  this.history.go(-1);
-}
-
-function stripHash(url) {
-  const hashIndex = url.indexOf('#');
-  return hashIndex === -1 ? url : url.slice(0, hashIndex);
-}
+export default createHistory;
---
+++ packages/history/src/utils.js
@@ -0,0 +1,27 @@
+export function parsePath(fragment) {
+  let pathname = fragment || '/';
+  let search = '';
+  let hash = '';
+
+  const hashIndex = pathname.indexOf('#');
+  if (hashIndex !== -1) {
+    hash = pathname.substr(hashIndex);
+    pathname = pathname.substr(0, hashIndex);
+  }
+
+  const searchIndex = pathname.indexOf('?');
+  if (searchIndex !== -1) {
+    search = pathname.substr(searchIndex);
+    pathname = pathname.substr(0, searchIndex);
+  }
+
+  return {
+    pathname,
+    search: search === '?' ? '' : search,
+    hash: hash === '#' ? '' : hash
+  };
+}
+
+export function addLeadingSlash(path) {
+  return path.charAt(0) === '/' ? path : '/' + path;
+}
